35417f692f256a60fd3cda8d1d1e026b42830e48
amir mohammad ghazanfari <amghira1@gmail.com>
2025-04-29 15:01:47 +0330

    adding variables

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,7 +4,15 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        // step one: variables
+        Scanner scanner = new Scanner(System.in);
+        double balance;
+        boolean isRunning = true;
+        int choice ;
 
 
+        // last step: exit
+
+        scanner.close();
     }
 }

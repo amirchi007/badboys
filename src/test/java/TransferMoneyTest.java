24ce0fe96c8fc25fefb9651f37f9c7b28632dddf
amir mohammad ghazanfari <amghira1@gmail.com>
2025-04-29 19:05:53 +0330

    TransferMoneyTest fun's added

--- src/test/java/TransferMoneyTest.java
+++ src/test/java/TransferMoneyTest.java
@@ -1,5 +1,25 @@
+import org.junit.jupiter.api.Test;
+
 public class TransferMoneyTest {
-    // Transfer between two accounts
-    // raise and decrease correctly
-    // exception for wrong account
+    /*
+     Transfer between two accounts
+     raise and decrease correctly
+     exception for wrong account
+    */
+
+    @Test
+    void shouldTransferMoneySuccessfullyBetweenAccounts() {}
+
+    @Test
+    void shouldUpdateSenderAndReceiverBalanceAfterTransfer() {}
+
+    @Test
+    void shouldThrowErrorWhenReceiverAccountDoesNotExist() {}
+
+    @Test
+    void shouldThrowErrorWhenInsufficientBalanceForTransfer() {}
+
+    @Test
+    void shouldNotAllowTransferToItsAccount() {}
+
 }

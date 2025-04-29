04eefac90ee6e2e3715e117ef60ab56c55b49f18
amir mohammad ghazanfari <amghira1@gmail.com>
2025-04-29 19:36:35 +0330

    DepositWithdrawTest fun's added

--- src/test/java/DepositWithdrawTest.java
+++ src/test/java/DepositWithdrawTest.java
@@ -1,5 +1,24 @@
+import org.junit.jupiter.api.Test;
+
 public class DepositWithdrawTest {
-    // Deposit and Withdraw correctly
-    // not mines balance
-    // decrease money correctly
+    /*
+     Deposit and Withdraw correctly
+     not mines balance
+     decrease money correctly
+    */
+
+    @Test
+    void shouldIncreaseBalanceWhenDepositIsValid() {}
+
+    @Test
+    void shouldThrowExceptionWhenDepositIsNegativeAmount() {}
+
+    @Test
+    void shouldDecreaseBalanceWhenWithdrawingIsValid() {}
+
+    @Test
+    void shouldThrowExceptionWhenWithDrawingIsMoreThanBalance() {}
+
+    @Test
+    void shouldShowCorrectBalanceAfterAnyOperation() {}
 }

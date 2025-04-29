4310fa31c28dbca240b844b67b1554d74e9b8d4a
amir mohammad ghazanfari <amghira1@gmail.com>
2025-04-29 15:02:24 +0330

    display menu

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -10,6 +10,15 @@ public class Main {
         boolean isRunning = true;
         int choice ;
 
+        // step two display menu
+        System.out.println("______________");
+        System.out.println("Welcome To ATM");
+        System.out.println("______________");
+        System.out.println("1. Show Balance");
+        System.out.println("2. deposit");
+        System.out.println("3. withdraw");
+        System.out.println("3. Exit");
+        System.out.println("______________");
 
         // last step: exit
 
